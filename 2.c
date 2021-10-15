6917083a5e2c2d4846a7e0397fec16bb8a155fb8
opensourcefuture <1770747317@qq.com>
2021-10-15 19:19:55 +0800

    新文件：   1.c 	新文件：   2.c

---
+++ 2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+		char a[4]="111",b[6]="222";
+  		char c[3] = {'3','3'};
+		printf("a=%s\n",a);
+		printf("b=%s\n",b);
+		printf("c=%s\n",c);
+//		char c[] = strcat(a,b);
+		strcat(c,a);
+		c[4]='4';
+		printf("strcat(a,b)=%s\n",strcat(a,b));
+		printf("a'=%s\n",a);
+		printf("la=%lu\n",sizeof(a)/sizeof(a[0]));
+		printf("c'=%s\n",c);
+		return 0;
+}
+		
