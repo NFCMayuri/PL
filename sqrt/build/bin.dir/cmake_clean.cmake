754676ea7926cd013018d64f672be669fe7c87b0
DataEraser <MayuriNFC@outlook.com>
2021-10-17 09:11:36 +0800

    new file:   ../CMakeLists.txt 	new file:   ../include/a.h 	new file:   ../src/a.c 	new file:   ../src/main.c

---
+++ sqrt/build/bin.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin.exe"
+  "../bin.exe.manifest"
+  "../bin.pdb"
+  "CMakeFiles/../bin.dir/src/a.obj"
+  "CMakeFiles/../bin.dir/src/a.obj.d"
+  "CMakeFiles/../bin.dir/src/main.obj"
+  "CMakeFiles/../bin.dir/src/main.obj.d"
+  "lib../bin.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/../bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
