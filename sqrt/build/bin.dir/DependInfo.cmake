37a909b80ca86ccf47046281525f8523b7101962
Mayuri <1770747317@qq.com>
2021-10-17 09:11:36 +0800

    new file:   ../CMakeLists.txt 	new file:   ../include/a.h 	new file:   ../src/a.c 	new file:   ../src/main.c

---
+++ sqrt/build/bin.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Administrator/PL/sqrt/src/a.c" "bin.dir/src/a.obj" "gcc" "bin.dir/src/a.obj.d"
+  "C:/Users/Administrator/PL/sqrt/src/main.c" "bin.dir/src/main.obj" "gcc" "bin.dir/src/main.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
