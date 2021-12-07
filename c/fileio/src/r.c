0320a6790d8c8bbc3eaacc466e34890362f1a840
Mayuri <63649939+MayuriNFC@users.noreply.github.com>
2021-12-07 19:54:45 +0800

    Update r.c

--- c/fileio/src/r.c
+++ c/fileio/src/r.c
@@ -1,9 +1,15 @@
 #include "../include/head.h"
 int r(const char* filename){
     FILE *fp = fopen("file.txt","r");
-    char a[100];
-    fscanf(fp,"%s",a);
-    printf("%s",a);
+    int i;
+    long location=-2;
+    while(ftell(fp)!=-1 && ftell(fp)!=location){
+        //location=ftell(fp);
+        //printf("location:%ld       ",location);
+        char a[100];
+        fscanf(fp,"%s",a);
+        printf("%s \n\n",a);
+    }
     fclose(fp);
     return 0;
 }
