765464b0a74faa58ebf2baeb6a5ea2d6772ab54b
opensourcefuture <1770747317@qq.com>
2021-12-15 20:19:51 +0800

    modified:   link_table/build.sh  	modified:   link_table/include/head.h  	modified:   link_table/src/main.c  	new file:   link_test/build.sh  	modified:   link_test/src/main.c  	new file:   test/build.sh  	new file:   test/src/1  	new file:   test/src/main.c

--- c/link_table/src/main.c
+++ c/link_table/src/main.c
@@ -1,21 +1,6 @@
 #include "../include/head.h"
-int main(){
-    struct body_link {
-        void *data;
-        int type;
-        struct body_link *next;
-    };
-    struct head_link {
-        char *name;
-        struct body_link *head;
-        struct body_link **location;
-        int offset;
-        struct head_link *next;
-    };
-    return 0;
-}
 
-(struct head_link*)hcreate(){
+*head_link   hcreate() {
     struct head_link *temp;
     temp = (struct head_link*)(malloc(sizeof(struct head_link)));
     temp->head = (struct body_link*)(malloc(sizeof(struct body_link)));
@@ -23,12 +8,30 @@ int main(){
     temp->offset = 0;
     return temp;
 }
-int hadd(struct head_link *head_hl){
+int hadd(head_link *head_hl){
     struct head_link *temp;
     temp = (struct head_link*)(malloc(sizeof(struct head_link)));
     head_hl->next = temp;
-    temp->head = head_hl->head;
-    temp->location = (struct body_link*)(malloc(sizeof(struct body_link)));
+    temp->head = (struct body_link*)(malloc(sizeof(struct body_link)));
+    *(temp->location) = temp->head;
     temp->offset = (head_hl->offset+1);
+    return 0;
+}
+struct body_link {
+    void *data;
+    int type;
+    struct body_link *next;
+};
+struct head_link {
+    char *name;
+    struct body_link *head;
+    struct body_link **location;
+    int offset;
+    struct head_link *next;
+};
+
+int main(){
+    struct head_link *first = hcreate();
+    hadd(first);
     return 0;
 }
