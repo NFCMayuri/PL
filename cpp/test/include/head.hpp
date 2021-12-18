076f498ffb327e1addfb36c00d5d97700405ce2b
DataEraser <MayuriNFC@outlook.com>
2021-12-18 21:40:06 +0800

    new file:   CMakeLists.txt  	new file:   build.sh  	new file:   include/head.hpp  	new file:   include/onebot.h  	new file:   include/str.h  	new file:   src/main.cpp  	new file:   src/onebot.cpp  	new file:   src/str.cpp

---
+++ cpp/test/include/head.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+//extern "C"
+//{
+#include "../include/str.h"
+//}
+struct body_link{
+    int type;
+    void *data;
+    struct body_link *next;
+};
+struct head_link{
+    int type[2];
+    char *name;
+    struct body_link *head;
+    struct body_link **location;
+    struct test *next;
+};
+struct head_link* add(){
+    struct head_link *temp=(struct head_link*)malloc(sizeof(struct head_link));
+    return temp;
+}
