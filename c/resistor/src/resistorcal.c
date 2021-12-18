5a74e70b60355f5567fc4c4807de939ab494d1de
Mayuri <1770747317@qq.com>
2021-12-18 16:01:07 +0800

    貼贴
     	new file:   resistor/CMakeLists.txt
     	new file:   resistor/build.sh
     	new file:   resistor/include/resistorcal.h
     	new file:   resistor/src/main.c
     	new file:   resistor/src/resistorcal.c
     	new file:   ../go/r/01/main.go

---
+++ c/resistor/src/resistorcal.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../include/resistorcal.h"
+double resistorcal(double R1,double R2,char type){
+    switch (type){
+        case '1' : return (R1*R2)/(R1+R2); break;
+        case '2' : return (R1*R2)/(R1-R2); break;
+    }
+}
