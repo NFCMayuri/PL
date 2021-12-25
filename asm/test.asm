a0fa005372325d2c1fdf7d60b86f4c19f9026700
opensourcefuture <1770747317@qq.com>
2021-12-25 21:17:08 +0800

    modified:   asm/test.asm 	new file:   c/unicodetozh/1.c

--- asm/test.asm
+++ asm/test.asm
@@ -1,13 +1,13 @@
 global main
 
 main:
-	mov ebx, 1
-	mov ecx, 2
-	add ebx, ecx
+    mov ebx, 1
+    mov ecx, 2
+    add ebx, ecx
     
-	mov [a], ebx
-	mov eax, [a]
-	ret
+    mov [a], ebx
+    mov eax, [a]
+    ret
 
 section .data
 a   dw    0
