23d0dabc342ef15c98cb2ba05e24ce7f8c409c4f
opensourcefuture <1770747317@qq.com>
2021-12-30 18:10:39 +0800

    new file:   ../structbit/main.c 	new file:   ../uniconbit/main.c

---
+++ c/uniconbit/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    union test {
+    unsigned int a:1;
+    unsigned int b:2;
+    unsigned int c:1;
+    };
+    union test test2;
+    test2.a=1;
+    printf("%d\n",test2.a);
+}
