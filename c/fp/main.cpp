54ee6ef55c003dff1bff720de431b1723c9ce1b4
DataEraser <MayuriNFC@outlook.com>
2022-01-04 17:55:20 +0800

    new file:   main.c 	new file:   main.cpp

---
+++ c/fp/main.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstring>
+int min(int a,int b){
+    return a-b;
+}
+int mul(int a,int b){
+    return a*b;
+}
+int div(int a,int b){
+    return a/b;
+}
+int add(const char *a,const char *b){
+    char c[100];
+    sprintf(c,"%s%s",a,b);
+    return (int)(*c); 
+}
+int add(int a,int b){
+    return a+b;
+}
+
+int calc(int (*fp)(int a,int b),int a , int b){
+    return (*fp)(a,b);
+}
+
+int main(int argc,char *argv[]){
+    int a=1,b=2;
+    printf("%d %d=%d\n",a,b,calc(add,a,b));
+}
