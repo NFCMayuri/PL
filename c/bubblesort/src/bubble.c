75dbd2b487d1c3408630f429ee7fa6473f79d106
DataEraser <MayuriNFC@outlook.com>
2022-04-09 20:54:49 +0800

    fix compile error

--- c/bubblesort/src/bubble.c
+++ c/bubblesort/src/bubble.c
@@ -1,7 +1,10 @@
-#include "bubblesort.h"
+#include "../include/bubblesort.h"
 int main(){
     int a[] = {1,2,3,4,5,6,7,8,9,10};
-    bubblesort(a);
+    int i;
+    for (i=0;i<sizeof(a)/sizeof(a[0]);i++){
+        printf("%d\n",a[i]);
+    }
     return 0;
 }
 int *bubblesort(int *array){
@@ -15,4 +18,5 @@ int *bubblesort(int *array){
             }
         }
     }
+    return *array;
 }
