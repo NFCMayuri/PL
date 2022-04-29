b38bcd49259657af3b29496d08c089ad830d252a
DataEraser <MayuriNFC@outlook.com>
2022-04-29 11:17:43 +0800

    modified:   rewrite.sh

--- rewrite.sh
+++ rewrite.sh
@@ -1,2 +1,3 @@
 git filter-branch -f --env-filter "GIT_COMMITTER_NAME=DataEraser; GIT_COMMITTER_EMAIL=MayuriNFC@outlook.com;"
 git filter-branch -f --env-filter "GIT_AUTHOR_NAME=DataEraser; GIT_AUTHOR_EMAIL=MayuriNFC@outlook.com; "
+git filter-branch -f --env-filter "GIT_COMMITTER_NAME=DataEraser; "
