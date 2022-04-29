403075909962daec5f55a5c8ee0d126dcbd56d10
DataEraser <102341238+DataEraserC@users.noreply.github.com>
2022-04-29 11:21:05 +0800

    modified:   rewrite.sh

--- rewrite.sh
+++ rewrite.sh
@@ -1,3 +1,11 @@
 git filter-branch -f --env-filter "GIT_COMMITTER_NAME=DataEraser; GIT_COMMITTER_EMAIL=MayuriNFC@outlook.com;"
 git filter-branch -f --env-filter "GIT_AUTHOR_NAME=DataEraser; GIT_AUTHOR_EMAIL=MayuriNFC@outlook.com; "
 git filter-branch -f --env-filter "GIT_COMMITTER_NAME=DataEraser; "
+
+git filter-branch -f --env-filter "if [ $GIT_COMMITTER_NAME == ‘CommitName’ ]; then GIT_COMMITTER_NAME=‘Demo’; fi "
+
+git filter-branch -f --env-filter "if [ $GIT_AUTHOR_NAME == ‘AuthName’ ]; then GIT_AUTHOR_NAME=‘Demo’; fi "
+
+git filter-branch -f --env-filter "if [ $GIT_COMMITTER_EMAIL == ‘CommitEmail’ ]; then GIT_COMMITTER_EMAIL=‘Demo’; fi "
+
+git filter-branch -f --env-filter "if [ $GIT_AUTHOR_EMAIL == ‘AuthEmail’ ]; then GIT_AUTHOR_EMAIL=‘Demo’; fi "
