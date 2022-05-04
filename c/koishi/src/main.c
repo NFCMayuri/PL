d43c149cdcd66ff2ec60f49f41db54dc5959b0bd
DataEraser <102341238+DataEraserC@users.noreply.github.com>
2022-05-05 07:53:49 +0800

    optimize some function and type

--- c/koishi/src/main.c
+++ c/koishi/src/main.c
@@ -1,12 +1,13 @@
 #include "../include/koishi.h"
+extern LEVEL getlevel();
 int main(){
-    char *level[4]={"easy","normal","hard","extreme"};
+    char *level[4] = {"easy", "normal", "hard", "extreme"};
     LEVEL l;
-    l=getlevel();
-    while (l<1||l>4){
-        printf("the number you input(%d) is not listed\nplease input again:\n",l);
-        l=getlevel();
+    l = getlevel();
+    while (  l < 1  ||  l > 4  ){
+        printf("the number you input(%d) is not listed\nplease input again:\n", l);
+        l = getlevel();
     }
-    printf("you choose %d %s\n",l,level[l-1]);
+    printf("you choose %d %s\n", l, level[l-1]);
     return 0;
 }
---
+++ cpp/koishi/include/koishi.hpp
@@ -0,0 +1,13 @@
+#ifndef _KOISHI_HPP_
+#define _KOISHI_HPP_
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+typedef enum level {
+    easy=1,
+    normal,
+    hard,
+    extreme
+    }LEVEL;
+#endif
