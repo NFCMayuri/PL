b0b89c89011e3ba7e8bd73eb8e4109c44907fbd7
DataEraser <102341238+DataEraserC@users.noreply.github.com>
2022-05-17 21:53:36 +0800

    format code

--- c/socket/windows/request.c
+++ c/socket/windows/request.c
@@ -15,53 +15,52 @@ typedef void Fun(char *, int);
 FILE *fp = NULL;
 _Bool dowm_flag = 0;
 
-const char *format_headers = (
-	"GET %s HTTP/1.1\r\n"
-	"Host: %s\r\n"
-	"Connection: keep-alive\r\n"
-	"Accept: */*\r\n"
-	"%s"
-	"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47\r\n\r\n"
-);
+const char *format_headers = ("GET %s HTTP/1.1\r\n"
+							  "Host: %s\r\n"
+							  "Connection: keep-alive\r\n"
+							  "Accept: */*\r\n"
+							  "%s"
+							  "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47\r\n\r\n");
 
 // Get admin from url(String Slicing)
-void get_admin(const char * url, char *admin)
+void get_admin(const char *url, char *admin)
 {
-    int start = 7;
-    if (strstr(url, "https"))
-        start++;
-	char *r1 = strchr(url+start, ':');
-    char *r2 = strchr(url+start, '/');
-	int admin_len = r1 ? r1-url-start : r2 ? r2-url-start : strlen(url)-start;
+	int start = 7;
+	if (strstr(url, "https"))
+		start++;
+	char *r1 = strchr(url + start, ':');
+	char *r2 = strchr(url + start, '/');
+	int admin_len = r1 ? r1 - url - start : r2 ? r2 - url - start
+											   : strlen(url) - start;
 
-    strncpy(admin, url+start, admin_len);
+	strncpy(admin, url + start, admin_len);
 }
 
 void get_admin_ip(const char *url, char *ip)
 {
-    char admin[30] = {0};
+	char admin[30] = {0};
 	get_admin(url, admin);
 
-    struct hostent *phst = gethostbyname(admin);
-    struct in_addr * iddr = (struct in_addr*)phst->h_addr;
-    inet_ntoa(*iddr);
-    strcpy(ip, inet_ntoa(*iddr));
+	struct hostent *phst = gethostbyname(admin);
+	struct in_addr *iddr = (struct in_addr *)phst->h_addr;
+	inet_ntoa(*iddr);
+	strcpy(ip, inet_ntoa(*iddr));
 }
 
 // If url contians https, return 443, else return 80
-int get_url_port(const char * url)
+int get_url_port(const char *url)
 {
 	int port;
 	char *p;
 
-	if (p = strchr(url+7, ':'))
+	if (p = strchr(url + 7, ':'))
 	{
-		sscanf(p+1, "%d", &port);
+		sscanf(p + 1, "%d", &port);
 		return port;
 	}
-    
-    char *ret = strstr(url, "https");
-    return ret ? 443 : 80;
+
+	char *ret = strstr(url, "https");
+	return ret ? 443 : 80;
 }
 
 _Bool get_file_name(char *header, char *filename)
@@ -69,14 +68,16 @@ _Bool get_file_name(char *header, char *filename)
 	char str[200];
 	char *p;
 	p = strstr(header, "Content-Disposition");
-	if (p == NULL) return 0;
+	if (p == NULL)
+		return 0;
 	sscanf(p, "%*s %[^\n]", str);
 	p = strchr(str, '\'');
-	if (p == NULL) return 0;
+	if (p == NULL)
+		return 0;
 	p += 2;
-	strncpy(filename, p, strlen(p)-1);
-	filename[strlen(p)-1] = '\0';
-	
+	strncpy(filename, p, strlen(p) - 1);
+	filename[strlen(p) - 1] = '\0';
+
 	return 1;
 }
 
@@ -86,8 +87,8 @@ void check_type(const char *url, char *type, char *filename, int nameflag)
 
 	if (type == NULL)
 		return;
-	
-	for (int i=0; i<4; i++) 
+
+	for (int i = 0; i < 4; i++)
 	{
 		if (strstr(type, type_list[i]))
 			return;
@@ -99,8 +100,8 @@ void check_type(const char *url, char *type, char *filename, int nameflag)
 		char *q = strrchr(p, '?');
 		if (q)
 		{
-			strncpy(filename, p, q-p);
-			filename[q-p] = '\0';
+			strncpy(filename, p, q - p);
+			filename[q - p] = '\0';
 		}
 		else
 		{
@@ -112,7 +113,7 @@ void check_type(const char *url, char *type, char *filename, int nameflag)
 			char ext[10] = {0};
 			ext[0] = '.';
 
-			sscanf(type, "%*[^/]/%[a-zA-Z]", ext+1);
+			sscanf(type, "%*[^/]/%[a-zA-Z]", ext + 1);
 			strcat(filename, ext);
 		}
 	}
@@ -125,42 +126,41 @@ int request(const char *sUrl, Fun *fun)
 	struct sockaddr_in servaddr;
 	clock_t start, end;
 	SOCKET sockfd;
-	
-	char buf[MAXRECV+1] = {0};
-	char send_msg[strlen(format_headers) + strlen(sUrl)*2];
+
+	char buf[MAXRECV + 1] = {0};
+	char send_msg[strlen(format_headers) + strlen(sUrl) * 2];
 	char data_type[10] = "text/plain";
 	char header[2500] = {0};
 	char admin[30] = {0};
 	char filename[100];
-    char ip[20] = {0};
+	char ip[20] = {0};
 	_Bool chunked = 1;
 	int header_len = 0;
 	int cont_len = 0;
 	int data_len = 0;
 	int status = 0;
 	int r_len;
-    int port;
+	int port;
 	char *p;
 	char *q;
-	
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
 	port = get_url_port(sUrl);
-    get_admin(sUrl, admin);
+	get_admin(sUrl, admin);
 	get_admin_ip(sUrl, ip);
-	sprintf(send_msg, format_headers, sUrl, admin, \
-		strstr(admin, "i.pximg.net")?"referer: http://www.pixiv.net/\r\n":""
-	);
+	sprintf(send_msg, format_headers, sUrl, admin,
+			strstr(admin, "i.pximg.net") ? "referer: http://www.pixiv.net/\r\n" : "");
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	memset(&servaddr, 0, sizeof(servaddr));
 
 	servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr(PROXY_FLAG?PROXY_IP:ip);
-	servaddr.sin_port = htons(PROXY_FLAG?PROXY_PORT:port);
-    
+	servaddr.sin_addr.s_addr = inet_addr(PROXY_FLAG ? PROXY_IP : ip);
+	servaddr.sin_port = htons(PROXY_FLAG ? PROXY_PORT : port);
+
 	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 	send(sockfd, send_msg, strlen(send_msg), 0);
 
@@ -170,14 +170,14 @@ int request(const char *sUrl, Fun *fun)
 	{
 		memset(buf, 0, sizeof(buf));
 		r_len = recv(sockfd, buf, MAXBYTE, 0);
-		memmove(header+header_len, buf, r_len);
+		memmove(header + header_len, buf, r_len);
 		header_len += r_len;
 		p = strstr(header, "\r\n\r\n");
 		if (p)
 		{
 			p[0] = '\0';
 			p += 4;
-			data_len = header_len-(p-header);
+			data_len = header_len - (p - header);
 			break;
 		}
 	}
@@ -186,7 +186,7 @@ int request(const char *sUrl, Fun *fun)
 	if (q = strstr(header, "Content-Type"))
 		sscanf(q, "%*s %s", data_type);
 	check_type(sUrl, data_type, filename, get_file_name(header, filename));
-	
+
 	// print response
 	if (status != 200)
 		printf("\n%s\n\n", header);
@@ -199,15 +199,17 @@ int request(const char *sUrl, Fun *fun)
 		sscanf(p, "%*s %d", &cont_len);
 	}
 
-	while(1)
+	while (1)
 	{
 		if (!chunked)
 		{
-			if (data_len >= cont_len) break;
+			if (data_len >= cont_len)
+				break;
 		}
 		else
 		{
-			if (strstr(buf, "\r\n0\r\n\r\n")) break;
+			if (strstr(buf, "\r\n0\r\n\r\n"))
+				break;
 		}
 		r_len = recv(sockfd, buf, MAXRECV, 0);
 		buf[r_len] = '\0';
@@ -217,21 +219,20 @@ int request(const char *sUrl, Fun *fun)
 		{
 			end = clock();
 			printf("\r");
-			printf("downloading %d/%d bytes %.0f%% - %.1fs",\
-				data_len, cont_len, (data_len/(float)cont_len)*100, (end-start)/1000.0
-			);
+			printf("downloading %d/%d bytes %.0f%% - %.1fs",
+				   data_len, cont_len, (data_len / (float)cont_len) * 100, (end - start) / 1000.0);
 		}
 	}
 
 	fclose(fp);
 	closesocket(sockfd);
-    WSACleanup();
+	WSACleanup();
 
 	return status;
 }
 
 // Example callback function
-void process_data(char * str, int len)
+void process_data(char *str, int len)
 {
 	if (!dowm_flag)
 	{
@@ -248,7 +249,7 @@ void https_to_http(char **url)
 {
 	if (url[1][4] == 's')
 	{
-		memmove(url[1]+1, "http", 4);
+		memmove(url[1] + 1, "http", 4);
 		url[1]++;
 	}
 }
@@ -267,7 +268,7 @@ int main(int argc, char *argv[])
 
 	if (argc < 3 || !strstr(argv[2], "-b"))
 		system("chcp 65001");
-	
+
 	https_to_http(argv);
 	printf("\n");
 	start = clock();
@@ -276,7 +277,7 @@ int main(int argc, char *argv[])
 	// request(url, process_data);
 	end = clock();
 
-	printf("\n\nexecution time %f seconds\n", (end - start)/1000.0);
+	printf("\n\nexecution time %f seconds\n", (end - start) / 1000.0);
 
 	return 0;
 }
