3d1cd7a5c6d4b4ecc315ccad529c07d14e3ac6bd
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2022-10-23 00:32:53 +0800

    Add rust demo and Book Library in c

---
+++ c/Library/main.c
@@ -0,0 +1,95 @@
+// fishc
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100
+
+struct Date {
+  short year;
+  short month;
+  short day;
+};
+struct Book {
+  char title[128];
+  char author[40];
+  float price;
+  struct Date date;
+  char publisher[40];
+};
+void getInput(struct Book *book);
+void printBook(struct Book *book);
+void InitLibrary(struct Book *Library[]);
+void printLibrary(struct Book *Library[]);
+void releaseLibrary(struct Book *Library[]);
+void getInput(struct Book *book) {
+  printf("Book Title:\n");
+  scanf("%s", book->title);
+  printf("Book Author:\n");
+  scanf("%s", book->author);
+  printf("Book Price:\n");
+  scanf("%f", &(book->price));
+  printf("Book Publish Date(2000-12-1):\n");
+  scanf("%hd-%hd-%hd", &(book->date.year), &(book->date.month),
+        &(book->date.day));
+  printf("Book Publisher:\n");
+  scanf("%s", book->publisher);
+}
+void printBook(struct Book *book) {
+  printf("Book Title:\n%s\n", book->title);
+  printf("Book Author:\n%s\n", book->author);
+  printf("Book Price:\n%f\n", book->price);
+  printf("Book Publish Date(2000-12-1):\n%hd-%hd-%hd\n", book->date.year,
+         book->date.month, book->date.day);
+  printf("Book Publisher:\n%s\n", book->publisher);
+}
+void InitLibrary(struct Book *Library[]) {
+  for (int i = 0; i < MAX_SIZE; i++) {
+    Library[i] = NULL;
+  }
+}
+void printLibrary(struct Book *Library[]) {
+  for (int i = 0; i < MAX_SIZE; i++) {
+    if (Library[i] != NULL) {
+      printBook(Library[i]);
+      putchar('\n');
+    }
+  }
+}
+void releaseLibrary(struct Book *Library[]) {
+  for (int i = 0; i < MAX_SIZE; i++) {
+    if (Library[i] != NULL) {
+      free(Library[i]);
+    }
+  }
+}
+int main() {
+  struct Book *library[MAX_SIZE];
+  struct Book *ptr = NULL;
+  char ch;
+  int index = 0;
+  InitLibrary(library);
+  while (1) {
+    printf("Do you need to input information(Y/N):\n");
+    do {
+      ch = getchar();
+      // printf("You input %c\n",ch);
+    } while (ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n');
+
+    if (ch == 'Y' || ch == 'y') {
+      if (index < MAX_SIZE) {
+        ptr = (struct Book *)malloc(sizeof(struct Book));
+        getInput(ptr);
+        library[index] = ptr;
+        index++;
+        putchar('\n');
+      } else {
+        printf("Library is full,exiting...\n");
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+  printf("Printing Library......\n");
+  printLibrary(library);
+  releaseLibrary(library);
+}
