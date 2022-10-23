e94a2eb7ab3d923043eb1fe149ab96701bb8d337
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2022-10-23 12:20:13 +0800

    Add a mess

---
+++ html/11.js
@@ -0,0 +1,23 @@
+// document.getElementById("h").innerHTML = 456
+// // console.log(document.getElementsByClassName("a1"))
+// document.getElementsByClassName("a1")[0].innerHTML = 4
+// var revise = document.getElementsByClassName("a1")
+// revise[0].innerHTML = "revise[0]"
+// // console.log(document.getElementById("h").innerHTML)
+// // console.log(document.getElementById("h").innerText)
+// document.getElementsByClassName("a1")[0].style.background = "red"
+function a()
+{
+document.getElementById("h").innerHTML = 456
+}
+function b()
+{
+    document.getElementById("h").innerHTML = 123
+}
+function key()
+{
+    console.log(event.key)
+    if (event.key == ' ') {
+        alert("\" \"")
+    }
+}
