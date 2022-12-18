7bdae85184f309677a293bba855d3e5419e45a8d
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2022-12-18 22:47:21 +0800

    fix bugs and add loop in rust game guess_number

--- rust/guess_number/src/main.rs
+++ rust/guess_number/src/main.rs
@@ -4,18 +4,26 @@ use std::io; // prelude
 fn main() {
     println!("Guess the number");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("the secret number is {}", secret_number);
-    println!("Guess a number:");
-    let mut guess = String::new();
+    // println!("the secret number is {}", secret_number);
     // std::io::stdin().read_line(&mut guess).expect("reading error");
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("reading line error");
-    let guess: u32 = guess.trim().parse().expect("parse to number error");
-    println!("the number you guess is {}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => print!("Too small!"), // arm
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Guess a number:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("reading line error");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
+        println!("the number you guess is {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"), // arm
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
