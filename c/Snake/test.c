be0114d959f22f2644caa0353d9d08132a7b9ce8
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2022-12-18 17:01:38 +0800

    Test: macro in inline function

--- c/Snake/test.c
+++ c/Snake/test.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define gotoxy(y, x) printf("%c[%d;%df", 0x1B, ((y) + 1), ((x) + 1))
+#define PRINT(s) printf("%s", #s);
+inline void HELLO()
+{
+    PRINT(HELLO)
+}
+void HELLO();
 int main()
 {
-    system("stty -icanon");
-    system("clear");
-    printf("Hello, World! "
-           "\n0123456789\n1234567890\n2345678901\n3456789012\n4567890123\n56789"
-           "01234");
-    getchar();
-    gotoxy(3, 2);
-    printf("asdfdsasdf");
-    gotoxy(8, 9);
-    printf("00");
-    printf("\033c");
+    HELLO();
     return 0;
 }
